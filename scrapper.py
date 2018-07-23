4bbddab50c0e06ceef4a91bc79d584ef0aab18e4
Kim Younghoo <zerohoo.kim@gmail.com>
2018-07-24 02:10:04 +0900

    fngudie 정보수집

--- scrapper.py
+++ scrapper.py
@@ -12,16 +12,16 @@ from lxml import html
 
 import db
 from db import Quarter
-from utils import parse_float, parse_int
+from utils import parse_float, parse_int, first_or_none
 
 
 DAUM_BASIC = 'http://finance.daum.net/item/main.daum?code='
 NAVER_COMPANY = 'http://companyinfo.stock.naver.com/v1/company/c1010001.aspx?cmp_cd='
 NAVER_YEARLY = "http://companyinfo.stock.naver.com/v1/company/ajax/cF1001.aspx?cmp_cd=%s&fin_typ=0&freq_typ=Y"
 NAVER_QUARTERLY = "http://companyinfo.stock.naver.com/v1/company/ajax/cF1001.aspx?cmp_cd=%s&fin_typ=0&freq_typ=Q"
-#NAVER_YEARLY_JSON = "http://companyinfo.stock.naver.com/v1/company/cF3002.aspx?cmp_cd=%s&frq=0&rpt=0&finGubun=MAIN&frqTyp=0&cn="
 NAVER_JSON1 = 'http://companyinfo.stock.naver.com/v1/company/cF4002.aspx?cmp_cd=%s&frq=0&rpt=1&finGubun=MAIN&frqTyp=0&cn='
 NAVER = 'https://finance.naver.com/item/main.nhn?code='
+FNGUIDE = 'http://comp.fnguide.com/SVO2/ASP/SVD_main.asp?pGB=1&gicode=A'
 
 
 LAST_YEAR = str(datetime.now().year - 1)
@@ -163,30 +163,36 @@ def parse_quarterly(code):
     stock = db.save_stock(stock)
     stock.save_record()
 
+
 def parse_snowball(code):
     if not parse_basic(code):
         print('수집 실패')
         return
-    print('종목 {} 스노우볼...'.format(code))
-    url = NAVER_COMPANY + code
-    print('네이버 {}'.format(url))
-    tree = tree_from_url(url)
-    
-    element = tree.xpath('//*[@id="pArea"]/div[1]/div/table/tr[3]/td/dl/dt[2]/b')
-    if not element:
-        print('수집 실패')
-        return
-    bps = parse_int(element[0].text)
-    print('BPS: {}'.format(bps))
 
-    element = tree.xpath('//*[@id="pArea"]/div[1]/div/table/tr[3]/td/dl/dt[6]/b')
-    if element:
-        dividend_rate = parse_float(element[0].text)
-        print('배당률: {}'.format(dividend_rate))
-    else:
-        dividend_rate = 0
-        print('배당 수집 실패')
+    if not parse_fnguide(code):
+        print('FnGuide 수집실패')
+        return
+    # print('종목 {} 스노우볼...'.format(code))
+    # url = NAVER_COMPANY + code
+    # print('네이버 {}'.format(url))
+    # tree = tree_from_url(url)
     
+    # element = tree.xpath('//*[@id="pArea"]/div[1]/div/table/tr[3]/td/dl/dt[2]/b')
+    # if not element:
+    #     print('수집 실패')
+    #     return
+    # bps = parse_int(element[0].text)
+    # print('BPS: {}'.format(bps))
+
+    # element = tree.xpath('//*[@id="pArea"]/div[1]/div/table/tr[3]/td/dl/dt[6]/b')
+    # if element:
+    #     dividend_rate = parse_float(element[0].text)
+    #     print('배당률: {}'.format(dividend_rate))
+    # else:
+    #     dividend_rate = 0
+    #     print('배당 수집 실패')
+    
+    print('종목 {} 스노우볼...'.format(code))
     url = NAVER_YEARLY % (code)
     tree = tree_from_url(url)
 
@@ -244,8 +250,8 @@ def parse_snowball(code):
 
     stock = {
         'code': code,
-        'dividend_rate': dividend_rate,
-        'bps': bps,
+        #'dividend_rate': dividend_rate,
+        #'bps': bps,
         'ROEs': ROEs,
         'last_year_index': last_year_index,
         'PBRs': PBRs,
@@ -335,3 +341,85 @@ def parse_line(line: str, etf_type: str):
         return
     words = line.split(' ')
     parse_etf(words[-1], words[0], etf_type)
+
+
+def parse_fnguide(code: str):
+    print('종목 {} FnGuide...'.format(code))
+    url = FNGUIDE + code
+    print('FnGuide {}'.format(url))
+    tree = tree_from_url(url)
+    
+    title = first_or_none(tree.xpath('//*[@id="giName"]/text()'))
+    if not title:
+        return False
+    
+    groups = first_or_none(tree.xpath('//*[@id="compBody"]/div[1]/div[1]/p/span[1]/text()'))
+    groups = groups.split(' ')
+    group = groups[1]
+    
+    subgroup = first_or_none(tree.xpath('//*[@id="compBody"]/div[1]/div[1]/p/span[4]/text()'))
+    subgroup = subgroup.replace('\xa0', '')
+
+    closing_month = first_or_none(tree.xpath('//*[@id="compBody"]/div[1]/div[1]/p/span[6]/text()'))
+    closing_month = parse_int(closing_month.split(' ')[0][:-1])
+
+    #per = parse_float(first_or_none(tree.xpath('//*[@id="corp_group2"]/dl[1]/dd/text()')))
+    forward_per = parse_float(first_or_none(tree.xpath('//*[@id="corp_group2"]/dl[2]/dd/text()')))
+    group_per = parse_float(first_or_none(tree.xpath('//*[@id="corp_group2"]/dl[3]/dd/text()')))
+    
+    #pbr = parse_float(first_or_none(tree.xpath('//*[@id="corp_group2"]/dl[4]/dd/text()')))
+    dividend_rate = parse_float(first_or_none(tree.xpath('//*[@id="corp_group2"]/dl[5]/dd/text()')))
+    
+    relative_earning_rate = parse_float(first_or_none(tree.xpath('//*[@id="svdMainChartTxt13"]/text()')))
+    
+    momentums = tree.xpath('//*[@id="svdMainGrid1"]/table/tbody/tr[3]/td[1]/span/text()')
+    momentums = [parse_float(m) for m in momentums]
+
+    month1 = momentums[0] if len(momentums) >= 1 else 0
+    month3 = momentums[1] if len(momentums) >= 2 else 0
+    month6 = momentums[2] if len(momentums) >= 3 else 0
+    month12 = momentums[3] if len(momentums) >= 4 else 0
+    
+    foreigner_weight = parse_float(first_or_none(tree.xpath('//*[@id="svdMainGrid1"]/table/tbody/tr[3]/td[2]/text()')))
+
+    beta = parse_float(first_or_none(tree.xpath('//*[@id="svdMainGrid1"]/table/tbody/tr[4]/td[2]/text()')))
+
+    stocks = first_or_none(tree.xpath('//*[@id="svdMainGrid1"]/table/tbody/tr[5]/td[1]/text()'))
+    stocks = stocks.split('/ ')
+    has_preferred_stock = False if stocks[1] == '0' else True
+    
+    floating_rate = parse_float(first_or_none(tree.xpath('//*[@id="svdMainGrid1"]/table/tbody/tr[6]/td[2]/text()')))
+
+    YoY = parse_float(first_or_none(tree.xpath('//*[@id="svdMainGrid2"]/table/tbody/tr/td[4]/span/text()')))
+
+    consensus_point = parse_float(first_or_none(tree.xpath('//*[@id="svdMainGrid9"]/table/tbody/tr/td[1]/text()')))
+    consensus_price = parse_int(first_or_none(tree.xpath('//*[@id="svdMainGrid9"]/table/tbody/tr/td[2]/text()')))
+    consensus_count = parse_int(first_or_none(tree.xpath('//*[@id="svdMainGrid9"]/table/tbody/tr/td[5]/text()')))
+
+    bps = parse_int(first_or_none(tree.xpath('//*[@id="highlight_D_A"]/table/tbody/tr[19]/td[3]/text()')))
+
+    stock = {
+        'code': code,
+        'group': group,
+        'subgroup': subgroup,
+        'closing_month': closing_month,
+        'forward_per': forward_per,
+        'group_per': group_per,
+        'dividend_rate': dividend_rate,
+        'relative_earning_rate': relative_earning_rate,
+        'month1': month1,
+        'month3': month3,
+        'month6': month6,
+        'month12': month12,
+        'foreigner_weight': foreigner_weight,
+        'beta': beta,
+        'has_preferred_stock': has_preferred_stock,
+        'floating_rate': floating_rate,
+        'YoY': YoY,
+        'consensus_point': consensus_point,
+        'consensus_price': consensus_price,
+        'consensus_count': consensus_count,
+        'bps': bps
+    }
+    db.save_stock(stock)
+    return True
